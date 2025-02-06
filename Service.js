7ac9318ce8dc94a9944c433e33fba6b9cbf7985c
Igor Gancharenka <44129876+igorganch@users.noreply.github.com>
2025-02-06 12:38:19 -0500

    Update Service.js

--- Service.js
+++ Service.js
@@ -3,17 +3,23 @@ const Sequelize = require('sequelize');
 var fs = require('fs');
 
 
+if(process.env.NODE_ENV !== 'production'){
+    require("dotenv").config();
 
-
-var sequelize = new Sequelize('d5caduj88ekc4q', 'zjigteybxvtsbo', '4dd5e9b9729fe6c8d5bf9ca20051cc8c79531f530beb83faaa93365ac2eb3d3c', {
-    host: 'ec2-44-194-92-192.compute-1.amazonaws.com',
-    dialect: 'postgres',
-    port: 5432,
-    dialectOptions: {
-        ssl: { rejectUnauthorized: false }
-    },
-
-   });
+}
+var sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        port: process.env.DB_PORT,
+        dialectOptions: {
+            ssl: { rejectUnauthorized: false },
+        },
+    }
+);
 
 
 sequelize.authenticate().then(function(){
